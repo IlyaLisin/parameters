e301aaf275fc7babb0880e5f987dc2a1e75e99bf
Ilya Lisin <inkozzg@gmail.com>
2016-10-20 01:52:16 +0300

    подсчет параметров через контуры

--- parameters.py
+++ parameters.py
@@ -5,75 +5,36 @@ import cv2
 image = cv2.imread("images/0000.png")
 # define the list of boundaries
 
-boundaries = [
-    ([0, 0, 0], [212, 212, 212]),
-]
+# TODO сделать другую бинаризацию
 
-# loop over the boundaries
-for (lower, upper) in boundaries:
-    # create NumPy arrays from the boundaries
-    lower = np.array(lower, dtype="uint8")
-    upper = np.array(upper, dtype="uint8")
+mask = cv2.inRange(image, np.array([0,0,0], dtype="uint8"), np.array([100,100,100], dtype="uint8"))
+im = mask
 
-    # find the colors within the specified boundaries and apply
-    # the mask
-    mask = cv2.inRange(image, lower, upper)
- #   output = cv2.bitwise_and(image, image, mask=mask)
+ret,thresh = cv2.threshold(im,127,255,0)
+image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-#    cv2.floodFill(image, mask, 0,0)
- #   im = output
-    im = mask
-    # Setup SimpleBlobDetector parameters.
-    params = cv2.SimpleBlobDetector_Params()
+im_with_keypoints = cv2.drawContours(image, contours, 0, (200, 200, 200), 3)
 
-    # Change thresholds
-    params.minThreshold = 0
-    params.maxThreshold = 20000
+# CALCULATE PARAMETERS
+kernelCount = len(contours)
 
-    params.filterByColor = True
-    params.blobColor = 0
-    # Filter by Area.
-    params.filterByArea = True
-    params.minArea = 100
-    params.maxArea = 30000
+cntSquares = [cv2.contourArea(cnt) for cnt in contours]
 
-    # Filter by Circularity
-    params.filterByCircularity = True
-    params.minCircularity = 0
-    params.maxCircularity = 1
+minS = min(cntSquares)
+maxS = max(cntSquares)
 
-    # Filter by Convexity
-    params.filterByConvexity = True
-    params.minConvexity = 0
-    params.maxConvexity = 1
-
-    # Filter by Inertia
-    params.filterByInertia = True
-    params.minInertiaRatio = 0
-    params.maxInertiaRatio = 1
-
-    # Create a detector with the parameters
-    ver = (cv2.__version__).split('.')
-    if int(ver[0]) < 3:
-        detector = cv2.SimpleBlobDetector(params)
-    else:
-        detector = cv2.SimpleBlobDetector_create(params)
-
-    # Detect blobs.
-    keypoints = detector.detect(im)
-
-    # Draw detected blobs as red circles.
-    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-    # the size of the circle corresponds to the size of blob
-
-    im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0, 0, 255),
-                                          cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    # Show blobs
-    cv2.imshow("Keypoints", im_with_keypoints)
-    cv2.waitKey(0)
+averS = sum(cntSquares) / len(contours)
 
 
-    # show the images
-    #cv2.imshow("images", np.hstack([image, output]))
-    cv2.waitKey(0)
+cntPerimeters = [cv2.arcLength(cnt, True) for cnt in contours]
+
+minP = min(cntPerimeters)
+maxP = max(cntPerimeters)
+
+averP = sum(cntPerimeters) / len(contours)
+print (kernelCount)
+# TODO балл зерна, метод подсчета зерен, ГОСТ-5639-82, 3.4
+
+# Show
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
