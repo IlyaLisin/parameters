2bd824c3ea6aa6dd81a95c4ca821bae62d6814b1
Ilya Lisin <inkozzg@gmail.com>
2016-10-19 23:19:24 +0300

    standalone app

--- parameters.py
+++ parameters.py
@@ -1,53 +1,79 @@
-# import sys, cv2 as cv
-#
-# img = cv.imread(sys.argv[1], 1)
-#
-# cv.HoughCircles(img, cv.HOUGH_GRADIENT,1,20,
-#                             param1=50,param2=30,minRadius=0,maxRadius=0)
-#
-# cv.imshow('HoughCircles', img)
-#
-#
-#
-# cv.waitKey()
-
-#
-# import cv2
-# import numpy as np
-#
-# img = cv2.imread('1_1.bmp',0)
-# img = cv2.medianBlur(img,5)
-# cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-#
-# circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-#                             param1=50,param2=30,minRadius=0,maxRadius=0)
-#
-# circles = np.uint16(np.around(circles))
-# for i in circles[0,:]:
-#     # draw the outer circle
-#     cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-#     # draw the center of the circle
-#     cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-#
-# cv2.imshow('detected circles',cimg)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
-
-import numpy as np
 import numpy as np
 import cv2
-im = cv2.imread('1_7.bmp')
-imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(imgray,127,255,0)
-im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-cnt = contours[4]
-cv2.drawContours(im2, [cnt], 0, (0,255,0), 3)
+# load the image
+image = cv2.imread("images/0000.png")
+# define the list of boundaries
 
-print(len(contours))
+boundaries = [
+    ([0, 0, 0], [212, 212, 212]),
+]
 
-cv2.imshow('detected circles',im2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# loop over the boundaries
+for (lower, upper) in boundaries:
+    # create NumPy arrays from the boundaries
+    lower = np.array(lower, dtype="uint8")
+    upper = np.array(upper, dtype="uint8")
+
+    # find the colors within the specified boundaries and apply
+    # the mask
+    mask = cv2.inRange(image, lower, upper)
+ #   output = cv2.bitwise_and(image, image, mask=mask)
+
+#    cv2.floodFill(image, mask, 0,0)
+ #   im = output
+    im = mask
+    # Setup SimpleBlobDetector parameters.
+    params = cv2.SimpleBlobDetector_Params()
+
+    # Change thresholds
+    params.minThreshold = 0
+    params.maxThreshold = 20000
+
+    params.filterByColor = True
+    params.blobColor = 0
+    # Filter by Area.
+    params.filterByArea = True
+    params.minArea = 100
+    params.maxArea = 30000
+
+    # Filter by Circularity
+    params.filterByCircularity = True
+    params.minCircularity = 0
+    params.maxCircularity = 1
+
+    # Filter by Convexity
+    params.filterByConvexity = True
+    params.minConvexity = 0
+    params.maxConvexity = 1
+
+    # Filter by Inertia
+    params.filterByInertia = True
+    params.minInertiaRatio = 0
+    params.maxInertiaRatio = 1
+
+    # Create a detector with the parameters
+    ver = (cv2.__version__).split('.')
+    if int(ver[0]) < 3:
+        detector = cv2.SimpleBlobDetector(params)
+    else:
+        detector = cv2.SimpleBlobDetector_create(params)
+
+    # Detect blobs.
+    keypoints = detector.detect(im)
+
+    # Draw detected blobs as red circles.
+    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
+    # the size of the circle corresponds to the size of blob
+
+    im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0, 0, 255),
+                                          cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Show blobs
+    cv2.imshow("Keypoints", im_with_keypoints)
+    cv2.waitKey(0)
+
+
+    # show the images
+    #cv2.imshow("images", np.hstack([image, output]))
+    cv2.waitKey(0)
